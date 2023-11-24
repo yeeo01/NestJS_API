59cc3a9dac37019d11c13d1a6a97fe602ab11d09
yeeo01 <pyje000@naver.com>
2023-11-24 20:33:16 +0900

    ready typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [MoviesModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    MoviesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('DB_HOST:', process.env.DB_HOST);
+    console.log('DB_PORT:', process.env.DB_PORT);
+    // 이와 같이 필요한 환경 변수들을 출력할 수 있습니다.
+  }
+}
